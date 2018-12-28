39c73fdf8223c51f480cb77487e853fff46c8dc7
ewhitby <ewhitby@ualberta.ca>
2018-12-27 19:15:34 -0700

    Added comments

--- src/Classes/Printable.h
+++ src/Classes/Printable.h
@@ -5,8 +5,9 @@
 class Printable{
 
 public:
-    Printable(){};
+    Printable(){}; //Empty default constructor
     // Change to Printer struct, containing the header and info
+	// All are pure virtual functions which forces "IO" to override them.
     virtual string toString() = 0;
     virtual string toStringInfo() = 0;
     virtual string toStringHeader() = 0;
