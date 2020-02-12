cc9a0beee94306e75ba775a62c8190a9b9c5f5d6
ewhitby <ewhitby@ualberta.ca>
2020-02-12 10:17:57 -0700

    Updated fans for specific states.

--- V2/src/fan.cpp
+++ V2/src/fan.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "rtos.h"
 
+#include <math.h>
 #include <PwmOut.h>
 
 #include <pin_defs.h>
@@ -9,10 +10,10 @@
 #include <fc_state_machine.h>
 
 float fctemp;
+float fccurrent;
+float idealtemp;
 
 // Object declarations
-DigitalOut led1(LED1); //remove later
-DigitalOut led3(LED3); //REMOVE LATER
 PwmOut pwm_1(PWM_1);
 DigitalOut fan_switch_all(FAN_SWITCH_ALL);
 
@@ -20,20 +21,37 @@ void fan_thread() {
     // Loop and check alert conditions at 10 Hz
     while (true) {
       fctemp = get_analog_values().fctemp1;
-      // Set permanent fan period; adjust duty cycle only
       pwm_1.period(0.00004); // 40us fan period CHECK THIS
 
       // Choose fan behaviour based on current state
       // NOTE: pwm will only output if H2 STOP input signal is 3.3V
       switch(get_fc_state()){
-        case FC_INIT :
+        case STANDBY :
           fan_switch_all.write(false);
           break;
-        case FC_STARTUP :
+        case FC_CHARGE :
           fan_switch_all.write(true);
           pwm_1.write(LOW);
           break;
-        case FC_FAULT :
+        case FC_RUN :
+          idealtemp = 52.204*(1-exp(-0.010*fccurrent)+38.095);
+          if (fctemp>=60){
+            pwm_1.write(HIGH); // Temp should be less than 60 degrees always
+          }
+          else if (fctemp<=idealtemp){
+            pwm_1.write(LOW);
+          }
+          else if (fctemp>idealtemp && fctemp<=idealtemp+5) {
+            pwm_1.write(MEDIUM);
+          }
+          else if (fctemp>idealtemp+5){
+            pwm_1.write(HIGH);
+          }
+          break;
+        case FC_SHUTDOWN :
+          fan_switch_all.write(false);
+          break;
+        case FC_ALARM :
           fan_switch_all.write(false);
           break;
         case FC_TEST :
@@ -51,6 +69,6 @@ void fan_thread() {
           pwm_1.write(LOW);
       }
 
-      ThisThread::sleep_for(1000);
+      ThisThread::sleep_for(0.2);
     }
 }
