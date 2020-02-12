15aea4033c7a22e381007dc050ea85002f900a7e
Reegan McAllister <mcallisterreegan@gmail.com>
2020-02-12 09:37:48 -0700

    Add WDT (Completely un-tested)

--- V2/src/main.cpp
+++ V2/src/main.cpp
@@ -5,7 +5,8 @@
 #include <data_logging.h>
 #include <fc_state_machine.h>
 
-
+// Watch dog timer timeout
+const uint32_t TIMEOUT_MS = 1000;
 
 Thread analog_read_thr;
 Thread error_checker_thr;
@@ -13,16 +14,21 @@ Thread data_logging_thr;
 Thread fc_state_machine_thr;
 
 int main() {
-    analog_read_thr.start(analog_read_thread);
-    
-    error_checker_thr.start(error_checker_thread);
-    
-    data_logging_thr.start(data_logging_thread);
 
+    // start watchdog timer
+    Watchdog &watchdog = Watchdog::get_instance();
+    watchdog.start(min(TIMEOUT_MS, get_max_timeout ()));
+    
+    // start threads
+    analog_read_thr.start(analog_read_thread);
+    error_checker_thr.start(error_checker_thread);
+    data_logging_thr.start(data_logging_thread);
     fc_state_machine_thr.start(fc_state_machine_thread);
 
     while (true) {
-     
+        // Reset watchdog timer
+        watchdog.kick();  // this is a little violent
+        
         wait(0.5);
     }
 }
